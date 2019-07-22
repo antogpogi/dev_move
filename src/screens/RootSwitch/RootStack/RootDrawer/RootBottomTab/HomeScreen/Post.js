5fd9f60cdc9a8fb5d3febb8a354789ba6a88e2eb
kierberdin <kierberdin>
2019-07-22 14:02:27 +0800

    editPost2

--- src/screens/RootSwitch/RootStack/RootDrawer/RootBottomTab/HomeScreen/Post.js
+++ src/screens/RootSwitch/RootStack/RootDrawer/RootBottomTab/HomeScreen/Post.js
@@ -11,7 +11,7 @@ import moment from 'moment-timezone';
 const Post = ({posts, openOption, like_unlike, comment_post, setComment, getComment, viewLikes, viewComments, viewProfile}) => {
 //   console.log(posts)
     return(
-        <View style={{flex: 1,}}>
+        <View style={{flex:1}}>
             <View style={styles.post}>
                 <View style={styles.head}>
                     <View style={[styles.user, styles.flexView]}>
@@ -28,10 +28,10 @@ const Post = ({posts, openOption, like_unlike, comment_post, setComment, getComm
                 </View>
                 <View style={styles.body}>
                     {/* <Image style={{width: "100%", height: "100%"}} source={{url: `${posts.post_media}`}} /> */}
-                    <Image style={{width: "100%", height: "70%", borderRadius: 10}} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
+                    <Image style={{width: "100%", height: "95%", borderRadius: 10}} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
                 </View>
                 <View style={styles.footer}>
-                    <View style={[styles.activities, styles.flexViewActivityBtn]}>
+                    <View style={styles.activities}>
                         <View style={styles.activityBtn}>
                             {/* {posts.is_liked == "1" ? 
                             <Text style={{color: "blue", padding: 13, }} onPress={() => like_unlike({post_id: posts.post_id, status: 0})}>L</Text>
@@ -52,20 +52,31 @@ const Post = ({posts, openOption, like_unlike, comment_post, setComment, getComm
                             <Text></Text>
                         </View>
                     </View>
-                    <View style={styles.view_comments}>
-                        <Image style={{width:33, height: 33, borderRadius: 35/2, left: 2, }} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
-                        <Text style={{marginLeft: '2%', bottom: '2%'}}>{posts.user_name}</Text>
-                        <View style={{right: '80%', top: '3%'}}>
-                        <Text style={{color: 'black'}}>Wow cool post, love it!</Text>
+                    <View style={styles.view_comments1}>
+                        <Image style={{ width: 33, height: 33, borderRadius: 35 / 2, left: 2, }} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
+                        <View style={{ flexDirection: 'column' }}>
+                            <Text style={{ marginLeft: '5%', bottom: '2%' }}>{posts.user_name}</Text>
+                            <View style={{ top: '2%', alignItems: 'flex-start', marginLeft: '5%', }}>
+                                <Text style={{ color: 'black' }}>Wow cool post, love it!</Text>
+                            </View>
                         </View>
                     </View>
-                    <View style={{height: '5%'}}></View>
-                    <View style={styles.view_comments}>
-                        <Image style={{width:33, height: 33, borderRadius: 35/2, left: 2, }} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
-                        <Text style={{marginLeft: '2%', bottom: '2%'}}>{posts.user_name}</Text>
-                        <View style={{right: '80%', top: '3%'}}>
-                        <Text style={{color: 'black'}}>Wow cool post, love it!</Text>
+                    {/* <View style={{height: '5%'}}></View> */}
+                    <View style={styles.view_comments1}>
+                        <Image style={{ width: 33, height: 33, borderRadius: 35 / 2, left: 2, }} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
+                        <View style={{ flexDirection: 'column' }}>
+                            <Text style={{ marginLeft: '5%', bottom: '2%' }}>{posts.user_name}</Text>
+                            <View style={{ top: '2%', alignItems: 'flex-start', marginLeft: '5%', }}>
+                                <Text style={{ color: 'black' }}>Wow cool post, love it!</Text>
+                            </View>
                         </View>
+                    </View>
+                    <View style={{paddingLeft: 20}}>
+                        <TouchableOpacity onPress={() => viewComments({post_id: posts.post_id})}>
+                            <Text style={{color: 'black', fontWeight: 'bold'}}>
+                                ...view all comments
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                     <View style={[styles.commentBox, styles.flexViewComment]}>
                         <Image style={{width:35, height: 35, borderRadius: 35/2, left: 2, }} source={require('../../../../../../../public/images/IMG_20190710_113534.jpg')} />
@@ -89,7 +100,8 @@ export default Post
 const styles = StyleSheet.create({
   post: {
       top: 20,
-      height: 530,
+    //   height: '70%',
+      height: 550,
     //   borderBottomColor: '#bbb', 
     //   borderBottomWidth: 2.5,
       borderRadius: 20,
@@ -99,15 +111,22 @@ const styles = StyleSheet.create({
   },
   head:{
     //   flex: 1,
-    paddingTop: 30
+    // top: '8%',
+    // backgroundColor: 'red'
   },
   body:{
-    //   flex:3,
-      padding: 15,
+      flex:2,
+      paddingTop: 10,
+      paddingLeft: 10,
+      paddingRight: 10,
+    //   backgroundColor:'blue'
+    //   top: 20
   },
   footer:{
-    //   flex:1,
-      bottom: '22%'
+      flex:2,
+      bottom: 5,
+      marginBottom: 10,
+    //   backgroundColor:'red'
   },
   flexView: {
       width: '100%',
@@ -118,8 +137,9 @@ const styles = StyleSheet.create({
     width: '60%',
     flexDirection: 'row',
     alignItems: 'center',
-    right: 15,
-    bottom: 10
+    // right: 15,
+    // bottom: 10
+    flex: 1
 },
   flexViewComment: {
     width: '90%',
@@ -127,44 +147,70 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1, borderRadius: 30,
     borderColor: 'gray',
-    left: '10%'
+    // left: '10%',
+    // backgroundColor: 'blue'
 },
   activities: {
       justifyContent: 'space-evenly',
-      height: 49
+      height: 30,
+      width: '55%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    // borderBottomWidth: 0.5,
+    // marginBottom: 10,
+    // right: 15,
+    bottom: 10,
+    flex: 1
 
   },
   commentBox:{
+    //   flex: 1,
       justifyContent: 'space-around',
+      marginLeft: '4%'
     //   marginTop: 5,
     //   alignItems: 'center'
     //   backgroundColor: 'red',
   },
   user:{
-      flex: 2,
+    //   flex: 2,
       justifyContent: 'space-around'
   },
   details:{
     //   flex:1,
       marginLeft: 15,
-      paddingTop: 30
+    //   paddingTop: 30
+    // top: 20
   },
   activityBtn:{
-    // flex: 1, 
+    flex: 2, 
     flexDirection: 'row',
     // justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   view_comments:{
+      flex: 1,
       marginLeft:15, 
       width:"90%", 
-      bottom: '3%',
+    //   bottom: '3%',
     //   marginBottom: '3%',
     //   backgroundColor: 'white',
     //   borderBottomColor: '#bbb', 
     //   borderBottomWidth: 1, 
       flexDirection:'row',
       alignItems:'center',
-    }
+    },
+    view_comments1:{
+        flex: 1,
+        marginLeft:15, 
+        width:"90%", 
+      //   bottom: '3%',
+      //   marginBottom: '3%',
+        // backgroundColor: 'white',
+      //   borderBottomColor: '#bbb', 
+      //   borderBottomWidth: 1, 
+        flexDirection:'row',
+        alignItems:'center',
+        marginBottom: 10
+      }
 });
 
